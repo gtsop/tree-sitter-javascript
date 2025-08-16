6775e85dd8f7332ffabc95d122cb010b39f150c8
gtsop <gtsop+github@protonmail.com>
2025-08-16 22:15:38 +0300

    initial commit

---
+++ grammar.js
@@ -0,0 +1,17 @@
+/**
+ * @file Javascript grammar
+ * @author George Tsopanoglou <gtsop+github@protonmail.com>
+ * @license AGPLv3
+ */
+
+/// <reference types="tree-sitter-cli/dsl" />
+// @ts-check
+
+module.exports = grammar({
+  name: "javascript",
+
+  rules: {
+    // TODO: add the actual grammar rules
+    source_file: $ => "hello"
+  }
+});
