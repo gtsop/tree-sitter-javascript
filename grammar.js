a2e0a94bf6d7c486950a69b17dedffceb633c8f7
george.tsopanoglou <george.tsopanoglou@dialecticanet.com>
2026-06-13 11:53:41 +0300

    feat: object binding within array binding

--- grammar.js
+++ grammar.js
@@ -292,6 +292,7 @@ module.exports = grammar({
         repeat(
           seq(
             choice(
+              $.object_binding,
               seq(optional(token("...")), $.identifier),
               $.assign,
               token(","),
